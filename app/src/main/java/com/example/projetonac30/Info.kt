f73e8e2d9714da5c7617e9de3cf2beccf8880719
Logon Aluno <logonaluno@local.com>
2020-03-05 12:28:13 -0300

    ending data persistence

--- app/src/main/java/com/example/projetonac30/Info.kt
+++ app/src/main/java/com/example/projetonac30/Info.kt
@@ -1,12 +1,36 @@
 package com.example.projetonac30
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_info.*
 
 class Info : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        var sh = getSharedPreferences("valores", Context.MODE_PRIVATE)
+
+        infoMagalu.text = intent.getStringExtra("valores")
+
+        limpar.setOnClickListener {
+            var intent = Intent(this,MainActivity::class.java)
+
+            sh.edit().clear().apply()
+
+
+            intent.setAction(Intent.ACTION_MAIN)
+
+            intent.addCategory(Intent.CATEGORY_HOME);
+
+
+
+            startActivity(intent);
+        }
+
+
     }
 }
--- app/src/main/java/com/example/projetonac30/MainActivity.kt
+++ app/src/main/java/com/example/projetonac30/MainActivity.kt
@@ -17,58 +17,101 @@ class MainActivity : AppCompatActivity() {
 
         var sh = getSharedPreferences("valores", Context.MODE_PRIVATE)
 
-        mercado_livre.setOnClickListener { v: View? ->
+        var dadosAmericanas = sh.getInt("americanas",0)
+        var dadosBuscape = sh.getInt("buscape",0)
+        var dadosEbay = sh.getInt("ebay",0)
+        var dadosMagalu = sh.getInt("magalu",0)
+        var dadosMercado = sh.getInt("mercado",0)
+        var dadosNetshoes = sh.getInt("netshoes",0)
+        var dadosSubmarino = sh.getInt("submarino",0)
+        var dadosWebmotors = sh.getInt("webmotors",0)
+
+
+
+
+        mercado_livre.setOnClickListener {
             var intent = Intent(this, MercadoLivre::class.java)
+            sh.edit().putInt("mercado", dadosMercado+1).apply()
+
+
 
             startActivity(intent);
 
         }
 
-        buscape.setOnClickListener { v: View? ->
+        buscape.setOnClickListener {
             var intent = Intent(this, Buscape::class.java)
+            sh.edit().putInt("buscape", dadosBuscape+1).apply()
 
             startActivity(intent);
 
         }
-        americanas.setOnClickListener { v: View? ->
+        americanas.setOnClickListener {
 
 
-//            sh.edit().putInt("vl", descricaoinfo.in)
+
 
             var intent = Intent(this, Americanas::class.java)
+            sh.edit().putInt("americanas", dadosAmericanas+1).apply()
 
             startActivity(intent);
 
         }
-        ebay.setOnClickListener { v: View? ->
+        ebay.setOnClickListener {
             var intent = Intent(this, Ebay::class.java)
 
+            sh.edit().putInt("ebay", dadosEbay+1).apply()
+
             startActivity(intent);
 
         }
-        magalu.setOnClickListener { v: View? ->
+        magalu.setOnClickListener {
             var intent = Intent(this, Magalu::class.java)
 
+            sh.edit().putInt("magalu", dadosMagalu+1).apply()
+
             startActivity(intent);
 
         }
-        netshoes.setOnClickListener { v: View? ->
+        netshoes.setOnClickListener {
             var intent = Intent(this, Netshoes::class.java)
 
+            sh.edit().putInt("netshoes", dadosNetshoes+1).apply()
+
             startActivity(intent);
 
         }
-        submarino.setOnClickListener { v: View? ->
+        submarino.setOnClickListener {
             var intent = Intent(this, Submarino::class.java)
 
+            sh.edit().putInt("submarino", dadosSubmarino+1).apply()
+
             startActivity(intent);
 
         }
-        webmotors.setOnClickListener { v: View? ->
+        webmotors.setOnClickListener {
             var intent = Intent(this, Webmotors::class.java)
 
+            sh.edit().putInt("webmotors", dadosWebmotors+1).apply()
+
             startActivity(intent);
 
         }
+
+
+        descricaoinfo.setOnClickListener{
+            var intent = Intent(this, Info::class.java)
+            intent.putExtra("valores", "Mercado Livre: " + dadosMercado + "\n"
+                    + "Ebay: " + dadosEbay + "\n"
+                    + "Buscape:" + dadosBuscape + "\n"
+                    + "Magazine Luiza:"+ dadosMagalu + "\n"
+                    + "Americanas:"+ dadosAmericanas + "\n"
+                    + "Netshoes:"+ dadosNetshoes + "\n"
+                    + "Submarino:"+ dadosSubmarino + "\n"
+                    +"Webmotors:"+ dadosWebmotors)
+
+            startActivity(intent)
+        }
+
     }
 }
